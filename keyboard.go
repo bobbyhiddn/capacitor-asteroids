b0ce80197086e7fa5e6bf7c7c5a7bc87ac845e11
sjpratt19 <sjpratt19@gmail.com>
2021-08-15 11:58:42 -0700

    Project init, added player movement

---
+++ keyboard.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"math"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func keyboard_handling() {
+	if ebiten.IsKeyPressed(ebiten.KeyUp) || ebiten.IsKeyPressed(ebiten.KeyW) { // Z Azerty
+		player_pos_x = player_pos_x + player_delta_x
+		player_pos_y = player_pos_y + player_delta_y
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyDown) || ebiten.IsKeyPressed(ebiten.KeyS) { // S
+		player_pos_x = player_pos_x - player_delta_x
+		player_pos_y = player_pos_y - player_delta_y
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) || ebiten.IsKeyPressed(ebiten.KeyA) { // Q Azerty
+		player_angle -= 0.10
+		// Reset
+		if player_angle <= 0 {
+			player_angle = 6.283
+		}
+		player_delta_x = math.Cos(player_angle) * 5
+		player_delta_y = math.Sin(player_angle) * 5
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) || ebiten.IsKeyPressed(ebiten.KeyD) { // D
+		player_angle += 0.10
+		// Reset
+		if player_angle >= 6.283 {
+			player_angle = 0
+		}
+		player_delta_x = math.Cos(player_angle) * 5
+		player_delta_y = math.Sin(player_angle) * 5
+	}
+	/*
+	if IsKeyTriggered(ebiten.KeyM) == true {
+		if STATE_SHOW_2D_MAP == 0 {
+			STATE_SHOW_2D_MAP = 1
+			STATE_MINIMAP = 0
+			x3d_orig = 530
+		} else {
+			STATE_SHOW_2D_MAP = 0
+			STATE_MINIMAP = 1
+			x3d_orig = 0
+		}
+	}
+	if IsKeyTriggered(ebiten.KeyJ) == true {
+		if STATE_MINIMAP == 0 {
+			STATE_MINIMAP = 1
+		} else {
+			STATE_MINIMAP = 0
+		}
+	}
+	if IsKeyTriggered(ebiten.KeyL) == true {
+		if STATE_SCANLINES == 0 {
+			STATE_SCANLINES = 1
+		} else {
+			STATE_SCANLINES = 0
+		}
+	}
+	if IsKeyTriggered(ebiten.KeyF) == true {
+		if STATE_FULLSCREEN == 0 {
+			ebiten.SetFullscreen(true)
+			STATE_FULLSCREEN = 1
+		} else {
+			ebiten.SetFullscreen(false)
+			STATE_FULLSCREEN = 0
+		}
+	}
+	if IsKeyTriggered(ebiten.KeyI) == true {
+		if STATE_SHOW_DEBUG == 0 {
+			STATE_SHOW_DEBUG = 1
+		} else {
+			STATE_SHOW_DEBUG = 0
+		}
+	}
+	if IsKeyTriggered(ebiten.KeyEnter) == true {
+		boot = 0
+	}*/
+}
